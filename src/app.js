94e1f23432a6ecd4cba618e18d6b7979aa3f4212
David Betancourt <dmarquez.develop@gmail.com>
2025-06-19 01:23:48 +0000

    using global variables inside window onload funtion

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
@@ -10,22 +9,17 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-function ramdonElement(array){
-  let ramdonIndex = Math.floor(Math.random() * array.length);
+window.onload = function () {
+  function ramdonElement(array) {
+    let ramdonIndex = Math.floor(Math.random() * array.length);
+    return array[ramdonIndex];
+  }
 
-  return array[ramdonIndex];
-}
+  function generateExcuse() {
 
-function generateExcuse(){
-  let subject = ramdonElement(who);
-  let verb = ramdonElement(action);
-  let object = ramdonElement(what);
-  let time = ramdonElement(when);
+    return `${ramdonElement(who)} ${ramdonElement(action)} ${ramdonElement(what)} ${ramdonElement(when)}`;
+  }
 
-  return `${subject} ${verb} ${object} ${time}.`;
-}
-
-window.onload = function() {
   let excuse = generateExcuse();
   document.getElementById('excuse').innerHTML = excuse;
 };
